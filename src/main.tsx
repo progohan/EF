1b762f728a13457aad54bfecd3f1828bbc51f434
progohan <efernandez19980@gmail.com>
2026-04-26 10:31:30 -0400

    perf: defer leaflet styles to project map chunk

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ErrorBoundary } from './components/ErrorBoundary.tsx'
 import './index.css'
-import 'leaflet/dist/leaflet.css'
 import App from './App.tsx'
 import { ThemeProvider } from "./components/theme-provider"
 
